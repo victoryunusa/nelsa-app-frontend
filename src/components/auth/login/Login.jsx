82186162983052f37276e12770876e91ed9a5ab6
Yunusa Victor <victoryunusa@Yunusas-MacBook-Pro.local>
2022-02-25 06:18:55 +0100

    Added: Login and Logout  functionality

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,9 +1,42 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+    const BaseUrl = process.env.REACT_APP_BASE_API_URL;
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (localStorage.getItem('user-info')){
+            navigate('/', { replace: true });
+        }
+
+    }, [])
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
+
+    async function login(){
+        const user = {email, password}
+        //console.warn(user)
+
+        
+
+        let result = await fetch(`${BaseUrl}/api/login`, {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        });
+
+        result = await result.json();
+        localStorage.setItem('user-info', JSON.stringify(result));
+        navigate('/', { replace: true });
+
+    }
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <div className="py-10 mt-0">
@@ -17,24 +50,20 @@ const Login = () => {
             <div className="px-8 py-6 mt-0 text-left bg-white shadow-md rounded-md w-3/12">
                 <h3 className="text-xl font-semibold text-neutral-500 mb-2">Login to your <span className="text-green-500">account</span></h3>
                 <p className="mt-1 text-xs text-neutral-500 mb-8">We are delighted to have you back.</p>
-                <form action="">
                     <div className="mt-4">
                         <div className="mt-4">
                             <label className="block text-neutral-500 text-sm font-semibold">Email</label>
-                                    <input type="text" placeholder="Email"
-                                        className="w-full px-4 py-3 mt-1 border text-neutral-500 text-xs rounded focus:outline-none focus:ring-1 focus:ring-green-500" />
+                                    <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-4 py-3 mt-1 border text-neutral-500 text-xs rounded focus:outline-none focus:ring-1 focus:ring-green-500" />
                         </div>
                         <div className="mt-4">
-                            <label class="block text-neutral-500 text-sm font-semibold">Password</label>
-                                    <input type="password" placeholder="Password"
-                                        className="w-full px-4 py-3 mt-1 border text-neutral-500 text-xs rounded focus:outline-none focus:ring-1 focus:ring-green-500" />
+                            <label className="block text-neutral-500 text-sm font-semibold">Password</label>
+                                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-4 py-3 mt-1 border text-neutral-500 text-xs rounded focus:outline-none focus:ring-1 focus:ring-green-500" />
                                     <Link to="/reset" className=" text-xs text-neutral-500 hover:text-green-500">Already have an account?</Link>
                         </div>
                         <div className="flex items-baseline justify-between">
-                            <button className="w-full px-4 py-3 mt-8 font-semibold text-white bg-green-500 rounded hover:bg-green-600">Login</button>
+                            <button onClick={login} className="w-full px-4 py-3 mt-8 font-semibold text-white bg-green-500 rounded hover:bg-green-600">Login</button>
                         </div>
                     </div>
-                </form>
                 <p className="mt-4 text-xs text-neutral-500">Already have an account?</p>
             </div>
         </div>
