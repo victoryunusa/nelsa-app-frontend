9e3e323692bd5fc36e7a214e00cdf97646f849a2
Yunusa Victor <victoryunusa@Yunusas-MacBook-Pro.local>
2022-03-15 13:05:23 +0100

    Fixed sticky sidebar and and updated products page

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -21,31 +21,34 @@ const Product = ({ product }) => {
   };
   let dollarUSLocale = Intl.NumberFormat("en-US");
   return (
-    <div className="bg-white rounded-md p-5 shadow-md">
-      <div className="flex justify-center items-center">
-        <img className="h-15" src={image} alt={product.name} />
+    <div class="px-3 py-3 flex flex-col border rounded-md justify-between">
+      <div>
+        <div class="font-bold text-gray-800">{product.name}</div>
+        <span class="font-light text-sm text-gray-400">150g</span>
       </div>
-      <h4 className="text-xs font-semibold">{product.name}</h4>
-      <div className="flex flex-row justify-between items-center gap-2 ">
-        <div className="text-sm font-bold">
+      <div class="flex flex-row justify-between items-center">
+        <span class="self-end font-bold text-lg text-teal-900">
           ₦ {dollarUSLocale.format(Math.round(product.price))}
-        </div>
+        </span>
+        <img
+          src={image}
+          className=" h-14 w-14 object-cover rounded-md"
+          alt={product.name}
+        />
       </div>
       <div className="mt-2">
         {itemIndex ? (
-          <div className="flex w-full items-center justify-between">
+          <div class="w-full flex justify-between">
             <button
-              className="text-white bg-green-500 px-3 py-1 rounded-md hover:bg-teal-900"
               onClick={() => handleDecreaseCart(product)}
+              className="px-3 py-1 w-14 text-white rounded-md bg-green-500 cursor-pointer"
             >
               -
             </button>
-            <span className="text-xs w-4" name="count" type="number">
-              {itemIndex.cartQuantity}
-            </span>
+            <span className="font-semibold mx-4">{itemIndex.cartQuantity}</span>
             <button
-              className="text-white bg-green-500 px-3 py-1 rounded-md hover:bg-teal-900"
               onClick={() => handleAddToCart(product)}
+              className="px-3 py-1 w-14 text-white rounded-md bg-green-500 cursor-pointer"
             >
               +
             </button>
@@ -66,3 +69,47 @@ const Product = ({ product }) => {
 };
 
 export default Product;
+
+{
+  /* <div className="bg-white rounded-md p-5 shadow-md m-2.5">
+  <div className="flex justify-center items-center">
+    <img className="h-15" src={image} alt={product.name} />
+  </div>
+  <h4 className="text-xs font-semibold">{product.name}</h4>
+  <div className="flex flex-row justify-between items-center gap-2 ">
+    <div className="text-sm font-bold">
+      ₦ {dollarUSLocale.format(Math.round(product.price))}
+    </div>
+  </div>
+  <div className="mt-2">
+    {itemIndex ? (
+      <div className="flex w-full items-center justify-between">
+        <button
+          className="text-white bg-green-500 px-3 py-1 rounded-md hover:bg-teal-900"
+          onClick={() => handleDecreaseCart(product)}
+        >
+          -
+        </button>
+        <span className="text-xs w-4" name="count" type="number">
+          {itemIndex.cartQuantity}
+        </span>
+        <button
+          className="text-white bg-green-500 px-3 py-1 rounded-md hover:bg-teal-900"
+          onClick={() => handleAddToCart(product)}
+        >
+          +
+        </button>
+      </div>
+    ) : (
+      <div className="flex items-center justify-center">
+        <button
+          className="text-white bg-green-500 w-full px-3 py-1 rounded-md hover:bg-teal-900"
+          onClick={() => handleAddToCart(product)}
+        >
+          <span className="text-xs">Add to cart</span>
+        </button>
+      </div>
+    )}
+  </div>
+</div>; */
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -21,23 +21,88 @@ const Products = () => {
   //   history.pushState(`/cart/${}?qty=${qty}`);
   // };
   return (
-    <div className="container">
-      <div className="flex mb-10">
+    <div class="container mx-auto">
+      <div className="flex pb-10">
         <div className="flex flex-col">
-          <h1 className="text-2xl font-semibold ">Order Now</h1>
+          <h1 className="text-2xl font-semibold">Order</h1>
         </div>
       </div>
+      <div class="flex lg:flex-row flex-col-reverse border bg-white rounded-md p-2.5">
+        <div class="w-full lg:w-3/5 min-h-screen">
+          <div class="flex flex-row justify-between items-center px-5 mt-5">
+            <div class="text-gray-800">
+              <div class="font-bold text-xl">Select items</div>
+              <span class="text-xs">Location ID#SIMON123</span>
+            </div>
+            <div class="flex items-center">
+              <div class="text-sm text-center mr-4">
+                <div class="font-light text-gray-500">last synced</div>
+                <span class="font-semibold">3 mins ago</span>
+              </div>
+              <div>
+                <span class="px-4 py-2 bg-gray-200 text-gray-800 font-semibold rounded">
+                  Help
+                </span>
+              </div>
+            </div>
+          </div>
 
-      <div className="flex flex-col md:flex-row gap-8">
-        <div className="w-4/6">
-          <div className="grid grid-flow-row md:grid-cols-5 flex-warp gap-6">
+          <div class="mt-5 flex w-full flex-row px-5 overflow-x-scroll md:overflow-none">
+            <span class="px-5 py-1 bg-teal-900 rounded-2xl text-white text-sm mr-4">
+              All items
+            </span>
+            <span class="px-5 py-1 rounded-2xl text-sm font-semibold mr-4">
+              Food
+            </span>
+            <span class="px-5 py-1 rounded-2xl text-sm font-semibold mr-4">
+              Cold Drinks
+            </span>
+            <span class="px-5 py-1 w-10 rounded-2xl text-sm font-semibold mr-4">
+              Hot Drinks
+            </span>
+          </div>
+
+          <div class="grid grid-cols-2 md:grid-cols-4 gap-4 px-5 mt-2.5 overflow-y-auto h-2/4">
             {products.map((product) => (
               <Product product={product} key={product.id} />
             ))}
           </div>
         </div>
-        <div className="w-2/6 rounded-md bg-white p-10">
+        <div class="w-full lg:w-2/5">
+          <div class="flex flex-row items-center justify-between px-5 mt-5">
+            <div class="font-bold text-xl">Current Order</div>
+            <div class="font-semibold">
+              <span class="px-4 py-2 rounded-md bg-red-100 text-red-500 mr-1">
+                Clear All
+              </span>
+              <span class="px-4 py-2 rounded-md bg-gray-100 text-gray-800">
+                Setting
+              </span>
+            </div>
+          </div>
           <Cart />
+          <div class="px-5 mt-5">
+            <div class="rounded-md shadow-lg px-4 py-4">
+              <div class="flex flex-row justify-between items-center">
+                <div class="flex flex-col">
+                  <span class="uppercase text-xs font-semibold">
+                    cashless credit
+                  </span>
+                  <span class="text-xl font-bold text-teal-900">$32.50</span>
+                  <span class=" text-xs text-gray-400 ">Available</span>
+                </div>
+                <div class="px-4 py-3 bg-gray-300 text-gray-800 rounded-md font-bold">
+                  {" "}
+                  Cancel
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="px-5 mt-5">
+            <div class="px-4 py-4 rounded-md shadow-lg text-center bg-teal-900 text-white font-semibold">
+              Pay With Cashless Credit
+            </div>
+          </div>
         </div>
       </div>
     </div>
